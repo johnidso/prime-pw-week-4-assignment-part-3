79fab8d9752a1ef82a512a2dd3e23908c25cdcf8
John Idso <john.d.idso@gmail.com>
2021-05-25 15:37:07 -0500

    core questions complete

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,37 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+let basket = [];
+
+function addItem(item) {
+  basket.push(item);
+  console.log(item, 'have been added to your basket.');
+  return true;
+}
+
+console.log('Basket includes', basket);
+console.log('adding apples, expect true:', addItem('apples'));
+console.log('Basket should now include apples', basket);
+addItem('snap peas');
+addItem('carrots');
+addItem('onions');
+addItem('avocados');
+console.log(basket);
+
+function listItems() {
+  for (i of basket) {
+    console.log(i);
+  }
+}
+
+listItems();
+
+function empty() {
+  basket = [];
+  return true;
+}
+
+
+console.log('emptying basket, expect true:', empty());
+console.log(basket);
